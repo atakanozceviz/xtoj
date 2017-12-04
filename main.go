755b4e3d8f0de5b2d1f541ea851474559b9eaddc
Atakan Ozceviz <atakanozceviz@hotmail.com>
2017-12-05 02:12:22 +0300

    now app auto redirects to https

--- main.go
+++ main.go
@@ -7,37 +7,45 @@ import (
 	"os"
 
 	"github.com/clbanning/mxj"
+	"github.com/unrolled/secure"
 )
 
 func main() {
 	PORT := os.Getenv("PORT")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		u := r.FormValue("url")
-		resp, err := http.Get(u)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		xml, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		mapVal, err := mxj.NewMapXml(xml, true)
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		js, err := mapVal.Json()
-		if err != nil {
-			io.WriteString(w, err.Error())
-			return
-		}
-
-		w.Write(js)
+	secureMiddleware := secure.New(secure.Options{
+		SSLRedirect:     true,
+		SSLProxyHeaders: map[string]string{"X-Forwarded-Proto": "https"},
 	})
-	http.ListenAndServe(":"+PORT, nil)
+
+	app := secureMiddleware.Handler(xtoj)
+	http.ListenAndServe(":"+PORT, app)
 }
+
+var xtoj = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	u := r.FormValue("url")
+	resp, err := http.Get(u)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	xml, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	mapVal, err := mxj.NewMapXml(xml, true)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	js, err := mapVal.Json()
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+
+	w.Write(js)
+})
